02dfa33fa5b4bf92933296195c5b89581e24d2e3
tomkim825 <tk825@hotmail.com>
2018-05-21 12:22:51 -0700

    I think it is final now

--- liri.js
+++ liri.js
@@ -7,6 +7,7 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var command = process.argv[2];
+// I think the next line is easier and cleaner than the "for loop" in activities
 var input = process.argv.splice(3).join(' ');
     if (input === undefined) {input=''};
 // need this command to make it work on my machine. I get errors:
@@ -16,9 +17,14 @@ var input = process.argv.splice(3).join(' ');
 //************************************************
 // **     Above reserved for initializing app     **
 //************************************************
+
 //---------------------------------------------------------
 // --      [start] 1st action for app -- clear console      --
 //---------------------------------------------------------
+// these lines adds separators into the log
+fs.appendFile('log.txt', "\n --------------------", function(err) {
+    if (err) { console.log(err)  }
+});
 // this command clears the console before displaying result [windows powershell users]
 process.stdout.write('\033c');
 // this command clears the console before displaying result [linux users... probably mac also. Does nothing on windows powershell]
@@ -26,6 +32,21 @@ console.log('\033[2J');
 //--------------------------------------------------
 // --         [end] 1st action - clear console       --
 //--------------------------------------------------
+
+///////////////////////////////////////
+// [start]   add command to log //
+/////////////////////////////////////
+function addLog(word){
+fs.appendFile('log.txt', "\n" + word, function(err) {
+    if (err) { console.log(err)  }
+});
+console.log(word);
+}
+addLog("You asked for: "+ command+' '+input+'\n');
+//////////////////////////////////////
+// [end]  add command to log //
+////////////////////////////////////
+
 //***************************************************************
 // ** [start] needCommand():  if  command is not entered   **
 //***************************************************************
@@ -44,6 +65,7 @@ if (command === undefined) {needCommand()};
 //****************************************************************
 // **[end]  needCommand() + run function if condition met  **
 //****************************************************************
+
 //////////////////////////////////////////
 //   [start] additionalCommand():   //
 ////////////////////////////////////////
@@ -53,6 +75,7 @@ function additionalCommand(){
 //////////////////////////////////////////
 //   [end]  additionalCommand():   //
 ////////////////////////////////////////
+
 //-----------------------------
 // --   [start] myTweets():   --
 //-----------------------------
@@ -60,9 +83,9 @@ function myTweets(){
     var params = {screen_name: 'tomkim825'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-        console.log('20 recent tweets from @'+params.screen_name+': \n');
+        addLog('20 recent tweets from @'+params.screen_name+': \n');
         for(var i=0; i<20; i++){
-            console.log('('+tweets[i].created_at.slice(0,16)+') '+tweets[i].text);
+            addLog('('+tweets[i].created_at.slice(0,16)+') '+tweets[i].text);
         };
         additionalCommand();
     } else { console.log(error) }
@@ -71,33 +94,35 @@ function myTweets(){
 //-----------------------------
 // --   [end] myTweets():   --
 //-----------------------------
+
 //**********************************
 // **   [start] spotifyThisSong():   **
 //**********************************
 function spotifyThisSong(input){
     if (input === ''){ 
         input = 'The Sign Ace of Base';
-        console.log('Please enter a track name to search. Some good ones are "Snow - Hey Oh", "Busy Child", or "One Week".   \nFor now, I pulled up info on "The Sign" by Ace of Base:\n')
+        addLog('Please enter a track name to search. Some good ones are "Snow - Hey Oh", "Busy Child", or "One Week".   \nFor now, I pulled up info on "The Sign" by Ace of Base:\n')
     };
     spotify.search({ type: 'track', query: input }, function(err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err + "\nPlease try again with a different query term");
+          return addLog('Error occurred: ' + err + "\nPlease try again with a different query term");
         }
-        console.log("Here are the top 3 results: ")
+        addLog("Here are the top 3 results: ")
         for( var i=0; i<3; i++){
-            console.log('\n-------------------------------- Result ' + (i+1) + ' --------------------------------');
-            console.log('Artist: ' + data.tracks.items[i].artists[0].name);
-            console.log('Song Name: ' + data.tracks.items[i].name);
-            console.log('Preview Link: ' +data.tracks.items[i].external_urls.spotify);
-            console.log('Album Name: '+data.tracks.items[i].album.name);
+            addLog('\n-------------------------------- Result ' + (i+1) + ' --------------------------------');
+            addLog('Artist: ' + data.tracks.items[i].artists[0].name);
+            addLog('Song Name: ' + data.tracks.items[i].name);
+            addLog('Preview Link: ' +data.tracks.items[i].external_urls.spotify);
+            addLog('Album Name: '+data.tracks.items[i].album.name);
         }
-        console.log('\n--------------------------------------------------------------------------');
+        addLog('\n--------------------------------------------------------------------------');
         additionalCommand();
       });
 }
 //**********************************
 // **   [end] spotifyThisSong():   **
 //**********************************
+
 ////////////////////////////////
 //    [start] movieThis():    //
 //////////////////////////////
@@ -105,26 +130,25 @@ function movieThis(input){
  
     if (input === ''){ 
         input = 'Mr. Nobody';
-        console.log('Please enter a movie to search. Some good ones are "Inception", "Avengers", or "Star Wars".   \nFor now, I pulled up info on "Mr. Nobody":\n')
+        addLog('Please enter a movie to search. Some good ones are "Inception", "Avengers", or "Star Wars".   \nFor now, I pulled up info on "Mr. Nobody":\n')
     };
-    console.log(input);
     // API CALL -- Then run a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function(error, response, body) {
         // If the request is successful
         if (!error && response.statusCode === 200) {
             // Parse the body of the site and recover just the imdbRating
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("Release Year: " + JSON.parse(body).Year);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Where Movie Was Produced (Country) : " + JSON.parse(body).Country);
-            console.log("Language of Movie: " + JSON.parse(body).Language);
-            console.log("Plot: " + JSON.parse(body).Plot);
-            console.log("Acotrs: " + JSON.parse(body).Actors);
+            addLog("Title: " + JSON.parse(body).Title);
+            addLog("Release Year: " + JSON.parse(body).Year);
+            addLog("IMDB Rating: " + JSON.parse(body).imdbRating);
+            addLog("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            addLog("Where Movie Was Produced (Country) : " + JSON.parse(body).Country);
+            addLog("Language of Movie: " + JSON.parse(body).Language);
+            addLog("Plot: " + JSON.parse(body).Plot);
+            addLog("Actors: " + JSON.parse(body).Actors);
             additionalCommand();
         } else{ 
-            console.log('Please try again using a different search term ')
+            addLog('Please try again using a different search term ')
         }
     });
     // End API call
@@ -132,16 +156,23 @@ function movieThis(input){
 ///////////////////////////////
 //    [end] movieThis():    //
 /////////////////////////////
+
+// *************************************
+// ** runLiri() - checks commands     *
+// **  and runs appropriate function *
+// *************************************
 function runLiri(){
-    // run appropriate function based on command
     // I considered using switch/case/break but in this situation, the following code was cleaner and compact
     if (command === 'my-tweets') { myTweets() }
     else if (command === 'spotify-this-song') { spotifyThisSong(input) }
     else if (command === 'movie-this') { movieThis(input) }
     else if (command === 'do-what-it-says') { doWhatItSays() }
-    else { console.log("I'm sorry, I don't recognize that command. Could it be misspelt?"); additionalCommand()}
+    else if ((command!==undefined) ){ console.log("I'm sorry, I don't recognize that command. Could it be misspelt?"); additionalCommand()}
     }
     runLiri();
+// ******************************
+// ** [end] runLiri() + run it     *
+// ******************************
 
 //---------------------------------
 // --   [start] doWhatItSays():   --
@@ -152,10 +183,10 @@ function doWhatItSays(){
             return console.log(error);
         } else{
             var dataArr = data.split(",");
-            command = dataArr[0]; 
-            input = dataArr[1];
+            command = dataArr[0].trim(); 
+            input = dataArr[1].trim();
+            addLog('Reading Random.txt.... \ncommand is: '+ command + "\ninput is: " + input + '\nLoading....\n')
             runLiri();        
-            console.log(command);
         }
 });
 }
